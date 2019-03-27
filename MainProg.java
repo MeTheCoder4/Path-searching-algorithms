5c4d3868b448ef1c566e160dde6390548e259a0d
MeThecCoder4 <ryszard.sinius@gmail.com>
2019-03-27 15:33:35 +0100

    A program can now draw the map from black and white images

--- MainProg.java
+++ MainProg.java
@@ -1,7 +1,8 @@
 public class MainProg {
     public static void main(String[] args) throws InterruptedException {
-        Map map = new Map("map01", 20);
-        FloodFill floodFill = new FloodFill(map, 5, 5);
+//        Map map = new Map("map01", 40);
+        Map map = new Map("poop.png");
+        FloodFill floodFill = new FloodFill(map, 10, 10);
         floodFill.run();
     }
 }
--- Map.java
+++ Map.java
@@ -1,4 +1,8 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,6 +12,36 @@ public class Map {
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_WHITE = "\u001B[37m";
     private static final String ANSI_BLUE = "\u001B[34m";
+
+    public Map() {}
+
+    public Map(String fileName) {
+        File imageFile = new File(fileName);
+        try {
+            BufferedImage image = ImageIO.read(imageFile);
+            this.mapLength = image.getWidth();
+            Raster raster = image.getData();
+            table = new int[mapLength][mapLength];
+
+            for(int i = 0; i < mapLength; i++) {
+                for(int j = 0; j < mapLength; j++) {
+                    int[] pixelData = new int[4];
+                    pixelData = raster.getPixel(i, j, pixelData);
+
+                    if(getPixelAverage(pixelData) < 125.0)
+                        table[j][i] = 2;
+                    else
+                        table[j][i] = 0;
+                }
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private double getPixelAverage(int[] pixelData) {
+        return (pixelData[0] + pixelData[1] + pixelData[2]) / 3.0;
+    }
 
     public Map(int mapLength) {
         this.mapLength = mapLength;
